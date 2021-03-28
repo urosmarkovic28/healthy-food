cb0abd4c4da5f445c4c11f7e37711dadea3dc50b
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-28 15:38:06 +0200

    Responsive

--- proizvodnja.php
+++ proizvodnja.php
@@ -1,3 +1,16 @@
 <?php include 'partials/single-headers.php' ?>
-<h1>Proizvodnja</h1>
+<!-- poruci -->
+<section class="poruci">
+    <article>
+        <img src="img/poruci1.jpg" alt="">
+    </article>
+    <article>
+        <img src="img/poruci2.jpg" alt="">
+    </article>
+    <article>
+        <h2>Poruci sveze</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>Poruci</button>
+    </article>
+</section>
 <?php include 'partials/footer.php' ?>
