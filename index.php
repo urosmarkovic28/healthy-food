0232909edc7a8b1ad3261f0e35a79b3d102912b3
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-28 13:54:39 +0200

    uvod html and css

--- index.php
+++ index.php
@@ -8,5 +8,19 @@
         <button>Procitajte vise</button>
     </div>
 </header>
-
+<!--uvod -->
+<section class="uvod container">
+    <article>
+        <h2>Koraci do <span>zdravlja</span></h2>
+        <h3>01</h3>
+        <h4>Dole ugljeni hidrati</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cum facilis inventore totam earum ipsa quasi. Accusamus cumque hic aperiam.</p>
+    </article>
+    <article>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus tempore natus consequatur inventore impedit assumenda fugiat laudantium aliquid quaerat provident.</p>
+        <h3>02</h3>
+        <h4>Gore proteini</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, sunt? Modi architecto reiciendis laboriosam hic non sunt saepe fuga, maiores quo iste, illum aut dolor eos repellat asperiores nulla possimus!</p>
+    </article>
+</section>
 <?php include 'partials/footer.php' ?>
